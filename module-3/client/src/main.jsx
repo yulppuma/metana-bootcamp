90184a587206d15a937cef751041ddcfc41a6c84
Yul Puma <yulppuma@gmail.com>
2025-06-22 23:43:35 -0700

    Started working on the frontend, got rainbowkit to work. Customizing the interface when connecting wallet. Once I get this down the rest should be fairly quick

---
+++ module-3/client/src/main.jsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { WagmiProvider, createConfig } from 'wagmi';
+import { http } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { getDefaultWallets, connectorsForWallets , RainbowKitProvider} from '@rainbow-me/rainbowkit';
+import { metaMaskWallet, coinbaseWallet, walletConnectWallet} from '@rainbow-me/rainbowkit/wallets';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const chains = [sepolia];
+const projectId = '957c573f4053485e517f52afcdd54506';
+const {connectors} = connectorsForWallets([
+    {
+      groupName: 'Recommended',
+      wallets: [metaMaskWallet, coinbaseWallet, walletConnectWallet],
+    },
+  ],
+  {
+    appName: 'ForgeERC1155Token',
+    projectId: projectId,
+  }
+);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient: http(sepolia.rpcUrls.default.http[0]),
+  chains,
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>,
+)
