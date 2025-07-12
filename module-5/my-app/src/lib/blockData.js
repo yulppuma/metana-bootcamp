b00e25441c2154f0070796ee6918819e07c76cbf
Yul Puma <yulppuma@gmail.com>
2025-07-11 18:00:30 -0700

    Updated getLogs call to retrieve more than a single block since it allows users to pull from a give range.

--- module-5/my-app/src/lib/blockData.js
+++ module-5/my-app/src/lib/blockData.js
@@ -15,40 +15,37 @@ export function blockData() {
       try {
         const latestBlockNumber = await alchemy.core.getBlockNumber();
         const blocks = [];
+        //single getLogs call for past 10 blocks for USDT transfers
+        const transferLogs = await alchemy.core.getLogs({
+            fromBlock: ethers.toBeHex(latestBlockNumber-9),
+            toBlock: ethers.toBeHex(latestBlockNumber),
+            address: [ERC20_TOKEN_ADDRESS],
+            topics: [TRANSFER_TOPIC],
+          });
 
+        const volumePerBlock = {}; //Need to convert to decimal
+        //parsing response and the total volume per block
+        for (let num = 0; num < transferLogs.length; num++) {
+          const blockNum = transferLogs[num].blockNumber;
+          const newVal = BigInt(transferLogs[num].data);
+          if (!volumePerBlock[blockNum]) volumePerBlock[blockNum] = 0n;
+          volumePerBlock[blockNum]+=newVal;
+        }
+        //populating first 10 blocks
         for (let i = 9; i >= 0; i--) {
           const blockNumber = latestBlockNumber - i;
           const block = await alchemy.core.getBlock(blockNumber);
           const baseFee = block.baseFeePerGas?.toString() || '0';
           const gasUsed = Number(block.gasUsed);
           const gasLimit = Number(block.gasLimit);
-
-          const transfers = await alchemy.core.getAssetTransfers({
-            fromBlock: `0x${blockNumber.toString(16)}`,
-            toBlock: `0x${blockNumber.toString(16)}`,
-            contractAddresses: [ERC20_TOKEN_ADDRESS],
-            category: ['erc20'],
-          });
-
-          const transferLogs = await alchemy.core.getLogs({
-            fromBlock: ethers.toBeHex(blockNumber),
-            toBlock: ethers.toBeHex(blockNumber),
-            address: [ERC20_TOKEN_ADDRESS],
-            topics: [TRANSFER_TOPIC],
-          });
-          let transferVolume = 0n;
-          for (let j = 0; j < transferLogs.length; j++) {
-            const val = BigInt(transferLogs[j].data);
-            transferVolume+=val;
-          }
-          const transferVolumeFormatted = ethers.formatUnits(transferVolume, USDT_DECIMALS);
+          const volumePerBlockFormatted = ethers.formatUnits(volumePerBlock[blockNumber], USDT_DECIMALS);
           blocks.push({
             number: blockNumber,
             baseFee: ethers.formatUnits(baseFee, 'gwei'),
             gasUsed,
             gasLimit,
             gasUsageRatio: Number(((gasUsed / gasLimit) * 100).toFixed(2)),
-            transferVolume: transferVolumeFormatted,
+            transferVolume: volumePerBlockFormatted,
             timestamp: block.timestamp,
           });
         }
