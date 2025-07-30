6ccfd4ae3ef3794dd86c635e17a53143be855488
Yul Puma <yulppuma@gmail.com>
2025-07-29 17:31:31 -0700

    Currently working on commit-reveal scheme. Since all mints will have to use this (presale, publicsale), i'm making sure this works first. As for the merkle proof, ill be handling that off-chain but will implement the verify functionality via isWhitelisted.

--- module-8/AdvanacedNFT/contracts/AdvancedNFT.sol
+++ module-8/AdvanacedNFT/contracts/AdvancedNFT.sol
@@ -1,48 +1,80 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.28;
+pragma solidity 0.8.30;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
-
-contract AdvancedNFT is ERC721 {
-    using BitMaps for Bitmaps.Bitmaps;
+contract AdvancedNFT is ERC721, Ownable2Step {
+    using BitMaps for Bitmaps.Bitmap;
+    using Strings for uint256;
 
     enum Stages {
-        isMintable,
+        NoMintsAllowed,
         Presale,
         PublicSale,
         SoldOut
     }
 
-    Stages public stage = Stages.isMintable;
+    //State Machine
+    Stages public stage = Stages.NoMintsAllowed;
 
+    //Merkle Tree and Airdrop Tracking
     bytes32 public immutable merkleRoot;
-    mapping(address => bool) hasMinted;
+    mapping(address => bool) whitelistHasMinted;
     BitMaps.BitMap private bitHasMinted;
 
-    uint8 tokensMinted;
+    //NFT metrics
+    uint256 public constant TOTAL_SUPPLY = 1000;
+    uint256 public tokensMinted;
 
-    constructor(bytes32 _merkleRoot) ERC721("AdvancedNFT", "ANFT"){
+    //States variables handling commit-reveal scheme
+    mapping(bytes32 => CommitDetails) public commitments;
+    uint256 public constant REVEAL_BLOCK_DELAY = 10;
+
+    //Pull pattern fund withdrawls
+    mapping(address => uint256) public pendingWithdrawals;
+
+    struct CommitDetails {
+        bytes32 commit;
+        uint256 commitBlock;
+        bool revealed;
+    }
+
+    constructor(bytes32 _merkleRoot) ERC721("AdvancedNFT", "ANFT") Ownable(msg.sender){
         merkleRoot = _merkleRoot;
     }
 
     modifier atStage(Stages _stage){
-        require(stage == _stage);
+        require(stage == _stage, "Invalid state");
         _;
     }
 
-    modifier transitionAfter(){
-        _;
-        nextStage();
+    function mint() public {
+        
     }
 
-    function verifyandExecute(bytes32[] calldata proof, address userAddress, uint256 bitIndex) internal view returns (bool){
+    function commit(bytes32 secret) public{
+        commitments[msg.sender].commit = secret;
+        commitments[msg.sender].commitBlock = block.number;
+    }
+
+    function reveal(bytes32 secret) public{
+
+    }
+
+    //Verify leaf
+    function isWhitelisted(bytes32[] calldata proof, address userAddress, uint256 bitIndex) public view returns (bool){
         bytes32 leaf = keccak256(abi.encodePacked(userAddress, bitIndex));
         return MerkleProof.verify(proof, merkleRoot, leaf);
+    }
+
+    function nextStage() internal{
+        stage = Stages(uint(stage) + 1);
     }
 }
