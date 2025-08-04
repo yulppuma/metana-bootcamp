8df0fd12d85185e6449cbc55e4b6f6f421f987dd
Yul Puma <yulppuma@gmail.com>
2025-08-04 11:15:14 -0700

    Finished testing AdvancedNFT.

--- module-8/AdvanacedNFT/test/AdvancedNFT.js
+++ module-8/AdvanacedNFT/test/AdvancedNFT.js
@@ -120,6 +120,69 @@ describe("AdvancedNFT", function () {
     expect(await advancedNFT.tokensMinted()).to.equal(1n);
   });
 
+  it("should revert if the user's commit has already been revealed", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await expect(advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") })).to.be.revertedWith("Already revealed");
+  });
+
+  it("should revert if the user reveals too early (before +10 blocks)", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+
+    await expect(advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") })).to.be.revertedWith("Too early for reveal");
+  });
+
+  it("should revert if the user reveals too late (before +256 blocks)", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(257);
+
+    await expect(advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") })).to.be.revertedWith("Too late for reveal");
+  });
+
+  it("should revert if the user reveal does not match what they originally used", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await expect(advancedNFT.connect(user).publicSaleMint(currentIndex++, commitHash, { value: ethers.parseEther("0.1") })).to.be.revertedWith("Incorrect secret");
+  });
+
   it("should revert mint with incorrect ether amount", async function () {
     await advancedNFT.nextStage(); // Presale
 
@@ -162,7 +225,7 @@ describe("AdvancedNFT", function () {
     ).to.be.revertedWith("Already claimed");
   });
 
-  /*it("should reach SoldOut state after all tokens minted", async function () {
+  it("should reach SoldOut state after all tokens minted", async function () {
     await advancedNFT.nextStage(); // Presale
 
     //First pre-sale mint
@@ -234,5 +297,161 @@ describe("AdvancedNFT", function () {
 
     expect(await advancedNFT.tokensMinted()).to.equal(1000n);
     expect(await advancedNFT.stage()).to.equal(3); // SoldOut
-  });*/
+  });
+
+  it("should allow owner to pull funds for respective contributors", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await advancedNFT.connect(owner).pullFunds([addr1.address],[ethers.parseEther("0.1")]);
+
+    expect(await advancedNFT.withdrawalAmount(addr1.address)).to.equal(ethers.parseEther("0.1"));
+  });
+
+  it("should revert if non-owner tries to pull funds for respective contributors", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await expect(advancedNFT.connect(addr1).pullFunds([addr1.address],[ethers.parseEther("0.1")])).to.be.revertedWithCustomError(advancedNFT, "OwnableUnauthorizedAccount");;
+  });
+
+  it("should revert if owner doesn't pull amount greater than 0", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await expect(advancedNFT.connect(owner).pullFunds([],[])).to.be.revertedWith("No amount/contributors");
+  });
+  it("should revert if receivers and amounts length are different", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await expect(advancedNFT.connect(owner).pullFunds([addr1.address, addr2.address],[ethers.parseEther("0")])).to.be.revertedWith("Length mismatch");
+  });
+
+  it("should revert if owner tries to pull more funds than the smart contract has balance for", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await expect(advancedNFT.connect(owner).pullFunds([addr1.address],[ethers.parseEther("0.2")])).to.be.revertedWith("Insufficient balance");
+  });
+
+  it("should allow contributor to withdraw funds using pull method", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await advancedNFT.connect(owner).pullFunds([addr1.address],[ethers.parseEther("0.1")]);
+
+    
+    expect(await advancedNFT.withdrawalAmount(addr1.address)).to.equal(ethers.parseEther("0.1"));
+
+    const beforeBalance = await ethers.provider.getBalance(addr1.address);
+    const withdrawTx = await advancedNFT.connect(addr1).withdrawFunds();
+    await withdrawTx.wait();
+    const contractAddress = await advancedNFT.getAddress();
+    expect (await ethers.provider.getBalance(contractAddress)).to.equal(0);
+    expect(await advancedNFT.withdrawalAmount(addr1.address)).to.equal(0);
+  });
+
+  it("should revert if contributor tries to pull funds when amount is less than 0", async function () {
+    await advancedNFT.nextStage(); // Presale
+    await advancedNFT.nextStage(); // PublicSale
+
+    const user = addrs[0];
+    const revealStr = "public_secret";
+    const revealHash = ethers.keccak256(ethers.toUtf8Bytes(revealStr));
+    const commitHash = await advancedNFT.connect(user).getHash(revealHash);
+
+    await advancedNFT.connect(user).commit(commitHash);
+    await advanceBlocks(11);
+
+    await advancedNFT.connect(user).publicSaleMint(currentIndex++, revealHash, { value: ethers.parseEther("0.1") });
+
+    expect(await advancedNFT.tokensMinted()).to.equal(1n);
+
+    await advancedNFT.connect(owner).pullFunds([addr1.address],[ethers.parseEther("0.1")]);
+
+    
+    expect(await advancedNFT.withdrawalAmount(addr1.address)).to.equal(ethers.parseEther("0.1"));
+
+    const beforeBalance = await ethers.provider.getBalance(addr1.address);
+    const withdrawTx = await advancedNFT.connect(addr1).withdrawFunds();
+    await withdrawTx.wait();
+    const contractAddress = await advancedNFT.getAddress();
+    expect (await ethers.provider.getBalance(contractAddress)).to.equal(0);
+    expect(await advancedNFT.withdrawalAmount(addr1.address)).to.equal(0);
+
+    await expect(advancedNFT.connect(addr1).withdrawFunds()).to.be.revertedWith("Nothing to withdraw");
+  });
 });
