41f2da5ecb94bc8aaa0195dabf4a202070eb56de
Yul Puma <yulppuma@gmail.com>
2025-09-26 13:44:52 -0700

    As per David's suggestion, I'm going to focus mainly on using an oracle and automation tool for this project. Therefore, I won't be using the ERC20Votes smart contract for governance features.

--- module-13-14/contracts/contracts/MyPriceFeed.sol
+++ module-13-14/contracts/contracts/MyPriceFeed.sol
@@ -5,11 +5,26 @@ import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.so
 
 contract MyPriceFeed{
 
-    constructor(){}
+    struct PriceFeed {
+        int256 answer;
+        uint8 decimals;
+    }
 
-    function getChainlinkDataFeedLatestAnswer(address tokenFeed) public view returns (int256) {
+    function getBatchDataFeed(address[] calldata feeds) external view returns (int256[] memory answers, uint8[] memory decimals){
+        uint256 n = feeds.length;
+        require(n > 0, "No price feed");
+        answers = new int256[](n);
+        decimals = new uint8[](n);
+        unchecked {
+            for (uint256 i = 0; i < n; ++i){
+                (answers[i], decimals[i]) = getDataFeed(feeds[i]);
+            }
+        }
+    }
+
+    function getDataFeed(address tokenFeed) internal view returns (int256, uint8 dec) {
         AggregatorV3Interface dataFeed = AggregatorV3Interface(tokenFeed);
         (, int256 answer, , ,) = dataFeed.latestRoundData();
-        return answer;
+        return (answer, dataFeed.decimals());
     }
 }
